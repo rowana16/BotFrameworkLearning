77ea314227113e7499b2c41d1a9d5c4fe4553d71
rowana16 <aaron.rowan@gmail.com>
2017-06-25 15:39:24 -0400

    Updated with Luis Config Ignored

---
+++ Bot Application1/Dialogs/LUISDialog.cs
@@ -0,0 +1,76 @@
+﻿using Bot_Application1.Models;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.FormFlow;
+using Microsoft.Bot.Builder.Luis;
+using Microsoft.Bot.Builder.Luis.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Bot_Application1.Dialogs
+{
+    [LuisModel("e37c1dee-4afa-4f04-bf32-41e0b557fa88", "bfd7f6c3a1514bacb3177ea4dc44dce9")]
+    [Serializable]
+    public class LUISDialog : LuisDialog<RoomReservation>
+    {
+        private readonly BuildFormDelegate<RoomReservation> ReserveRoom;
+
+        public LUISDialog(BuildFormDelegate<RoomReservation> reserveRoom)
+        {
+            this.ReserveRoom = reserveRoom;
+        }
+
+
+        [LuisIntent("")]
+        public async Task None(IDialogContext context, LuisResult result)
+        {
+            await context.PostAsync("I'm sorry I don't know what you mean.");
+            context.Wait(MessageReceived);
+        }
+
+        [LuisIntent("Greeting")]
+        public async Task Greeting(IDialogContext context, LuisResult result)
+        {
+            context.Call(new GreetingDialog(), Callback);
+        }
+
+        private async Task Callback(IDialogContext context, IAwaitable<object> result)
+        {
+            context.Wait(MessageReceived);
+        }
+
+        [LuisIntent("ReserveRoom")]
+        public async Task RoomReservation(IDialogContext context, LuisResult result)
+        {
+            var enrollmentForm = new FormDialog<RoomReservation>(new RoomReservation(), this.ReserveRoom, FormOptions.PromptInStart);
+            context.Call<RoomReservation>(enrollmentForm, Callback);
+        }
+
+        [LuisIntent("QueryAmenities")]
+        public async Task QueryAmenities(IDialogContext context, LuisResult result)
+        {
+            foreach (var entity in result.Entities.Where(Entity => Entity.Type == "Amenity"))
+            {
+                var value = entity.Entity.ToLower();
+                if (value == "pool" || value == "gym" || value == "wifi" || value == "towels")
+                {
+                    await context.PostAsync("Yes we have that!");
+                    context.Wait(MessageReceived);
+                    return;
+                }
+                else
+                {
+                    await context.PostAsync("I'm sorry we don't have that.");
+                    context.Wait(MessageReceived);
+                    return;
+                }
+            }
+            await context.PostAsync("I'm sorry we don't have that.");
+            context.Wait(MessageReceived);
+            return;
+        }
+
+    }
+}
